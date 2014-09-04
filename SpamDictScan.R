5c961f6ce38c25cb0c0b5feb78a52a9237a10c02
Joel Kreager <joel.kreager@gmail.com>
2014-09-04 14:49:58 -0700

    Revise to create dictionary from vectors as DF. (Incomplete)

--- SpamDictScan.R
+++ SpamDictScan.R
@@ -1,8 +1,3 @@
-# If the dictionary has been constructed with correct counts, then it will sum to 1
-# when the counts are divided by the correct denominators since the dictionary contains
-# all possible outcomes and so must sum to 1. I evidently must figure out how this works
-# to have the program output probabilities.
-
 scanFile <- function(fp) {
   words <- scan(fp, character(0), quote="", quiet=T)
   return(words)
@@ -46,52 +41,40 @@ Pham <- (1-Pspam)
 stop_words <- c('the','and','a','in','on','is','of','subject',
                 'to','at', ""
                )
+# Create vectors to store the counts and words for dictionary data frame.
+filename <- paste(file_path, "english_wordlist.txt", sep="/")
+english_words <- scanFile(filename)
+english_words <- tolower(english_words)
+spam_count <- rep(0, length(english_words))
+ham_count <- rep(0, length(english_words))
+total_count <- rep(0, length(english_words))
+names(spam_count) <- english_words
+names(ham_count) <- english_words
+names(total_count) <- english_words
 
-# Going to need a structure 
-# Try:
-# List[[word]][spam count][total count][training set: 0 or 1]
-# Change to:
-# List[[word][spam count][ham count][total count][word in dictionary T=1]
 #if(file.exists("EnglishDict.Rdata")) {
-#  load("EnglishDict.Rdata")
+#  print("file present")
+#  filename <- paste(file_path, "EnglishDict.Rdata", sep="/")
+#  load(filename)
 #  D <- EnglishDict
 #  english_words <- names(EnglishDict)
 #  rm(EnglishDict)
+# } else {
+# print("not present")
+##  # Try building this from a dictionary of the english language
+# filename <- paste(file_path, "english_wordlist.txt", sep="/")
+# english_words <- scanFile(filename)
+# english_words <- tolower(english_words)
+# EnglishDict <- list()
+# for(w in english_words) {
+#   EnglishDict[[w]] <- c(0, 0, 0, 1)
+#   cat(".")
+# }
+#  filename <- paste(save_path, "EnglishDict.Rdata", sep="/")
+#  save(EnglishDict, file=filename)
+#  D <- EnglishDict
+#  rm(EnglishDict)
 #}
-#else {
-#  # Try building this from a dictionary of the english language
-#  english_words <- scanFile("./english_wordlist.txt")
-#  EnglishDict <- list()
-#  for(w in english_words) {
-#    D[[w]] <- c(0, 0, 0, 1)
-#    #print(w)
-#  }
-#  save(EnglishDict, file="EnglishDict.Rdata")
-#}
-
-if(file.exists("EnglishDict.Rdata")) {
-  print("file present")
-  filename <- paste(file_path, "EnglishDict.Rdata", sep="/")
-  load(filename)
-  D <- EnglishDict
-  english_words <- names(EnglishDict)
-  rm(EnglishDict)
- } else {
- print("not present")
-#  # Try building this from a dictionary of the english language
- filename <- paste(file_path, "english_wordlist.txt", sep="/")
- english_words <- scanFile(filename)
- english_words <- tolower(english_words)
- EnglishDict <- list()
- for(w in english_words) {
-   EnglishDict[[w]] <- c(0, 0, 0, 1)
-   cat(".")
- }
-  filename <- paste(save_path, "EnglishDict.Rdata", sep="/")
-  save(EnglishDict, file=filename)
-  D <- EnglishDict
-  rm(EnglishDict)
-}
 
 for(i in 1:length(spam_files)) {
 #for(i in 1:5) {
@@ -101,16 +84,22 @@ for(i in 1:length(spam_files)) {
   words <- removePunct(words, stop_words)
   d <- words[words %in% english_words]
   nd <- words[!(words %in% english_words)]
-  for(w in d) {
-    # Spam count
-    D[[w]][1] <- D[[w]][1] + 1
-    # Total count
-    D[[w]][3] <- D[[w]][3] + 1
-    #cat(w, " in dictionary\n")
-  }
+  spam_count[d] <- spam_count[d] + 1
+  total_count[d] <- total_count[d] + 1
+#  for(w in d) {
+#    # Spam count
+#    D[[w]][1] <- D[[w]][1] + 1
+#    # Total count
+#    D[[w]][3] <- D[[w]][3] + 1
+#    #cat(w, " in dictionary\n")
+#  }
   for(w in nd) {
+    english_words[length(english_words) + 1L] <- w
+    spam_count[w] <- 1
+    total_count[w] <- 1
+    ham_count[w] <- 0
     # c(spamcount,hamcount,totalcount,indictionary)
-    D[[w]] <- c(1, 0, 1, 0)
+    #D[[w]] <- c(1, 0, 1, 0)
     #cat(w, " NOT in dictionary\n")
   }
   print(fp)
@@ -124,51 +113,57 @@ for(i in 1:length(ham_files)) {
   words <- removePunct(words, stop_words)
   d <- words[words %in% english_words]
   nd <- words[!(words %in% english_words)]
-  for(w in d) {
-    # Ham count
-    D[[w]][2] <- D[[w]][2] + 1
-    # Total count
-    D[[w]][3] <- D[[w]][3] + 1
-  }
+  ham_count[d] <- ham_count[d] + 1
+  total_count[d] <- total_count[d] + 1
+#  for(w in d) {
+#    # Ham count
+#    D[[w]][2] <- D[[w]][2] + 1
+#    # Total count
+#    D[[w]][3] <- D[[w]][3] + 1
+#  }
   for(w in nd) {
+    english_words[length(english_words) + 1L] <- w
+    spam_count[w] <- 0
+    total_count[w] <- 1
+    ham_count[w] <- 1
     # c(spamcount,hamcount,totalcount,indictionary)
-    D[[w]] <- c(0, 1, 1, 0)
+    #D[[w]] <- c(0, 1, 1, 0)
   }
   print(fp)
 }
 # Save everything
-filename <- paste(save_path, "spamDict2.Rdata", sep="/")
-save(D, WORDCOUNT, Nspam, Nham, Pspam, Pham, stop_words, file=filename)
-# Try creating a sorted data frame
-# Save variables and dictionary
-sdwords <- sort(names(D))
-# Only save tokens found in mincount emails
-mincount <- 5
-#mincount <- 2
-# Create vectors for data frame columns.
-email_words <- vector()
-spam_count <- vector()
-ham_count <- vector()
-total_count <- vector()
-
-for(i in 1:length(sdwords)) {
-#for(i in 1:10000) {
-  #print(D[[sdwords[i]]][3])
-  if(D[[sdwords[i]]][3] >= mincount){
-    print("found one")
-    #email_words[length(email_words)+1] <- sdwords[i]
-    email_words <- c(email_words, sdwords[i])
-    #spam_count[length(spam_count)+1] <- D[[sdwords[i]]][1]
-    spam_count <- c(spam_count, D[[sdwords[i]]][1])
-    #ham_count[length(ham_count)+1] <- D[[sdwords[i]]][2]
-    ham_count <- c(ham_count, D[[sdwords[i]]][2])
-    #total_count[length(total_count)+1] <- D[[sdwords[i]]][3]
-    total_count <- c(total_count, D[[sdwords[i]]][3])
-  }  
-}
+#filename <- paste(save_path, "spamDict2.Rdata", sep="/")
+#save(D, WORDCOUNT, Nspam, Nham, Pspam, Pham, stop_words, file=filename)
+## Try creating a sorted data frame
+## Save variables and dictionary
+#sdwords <- sort(names(D))
+## Only save tokens found in mincount emails
+#mincount <- 5
+##mincount <- 2
+## Create vectors for data frame columns.
+#email_words <- vector()
+#spam_count <- vector()
+#ham_count <- vector()
+#total_count <- vector()
+#
+#for(i in 1:length(sdwords)) {
+##for(i in 1:10000) {
+#  #print(D[[sdwords[i]]][3])
+#  if(D[[sdwords[i]]][3] >= mincount){
+#    print("found one")
+#    #email_words[length(email_words)+1] <- sdwords[i]
+#    email_words <- c(email_words, sdwords[i])
+#    #spam_count[length(spam_count)+1] <- D[[sdwords[i]]][1]
+#    spam_count <- c(spam_count, D[[sdwords[i]]][1])
+#    #ham_count[length(ham_count)+1] <- D[[sdwords[i]]][2]
+#    ham_count <- c(ham_count, D[[sdwords[i]]][2])
+#    #total_count[length(total_count)+1] <- D[[sdwords[i]]][3]
+#    total_count <- c(total_count, D[[sdwords[i]]][3])
+#  }  
+#}
 
 # create a dataframe from the vectors
-SpamDictionary <- data.frame(email_words, spam_count, ham_count, total_count, stringsAsFactors=FALSE)
+SpamDictionary <- data.frame(english_words, spam_count, ham_count, total_count, stringsAsFactors=FALSE)
 
-filename <- paste(save_path, "spamDictionaryDF.Rdata", sep="/")
-save(SpamDictionary, Nspam, Nham, stop_words, file=filename)
+#filename <- paste(save_path, "spamDictionaryDF.Rdata", sep="/")
+#save(SpamDictionary, Nspam, Nham, stop_words, file=filename)
